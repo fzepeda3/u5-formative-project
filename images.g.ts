2d06c4dec9fce8eed31bf4c93b534d85ccd3aca2
fzepeda3 <96065034+fzepeda3@users.noreply.github.com>
2021-12-13 08:37:20 -0600

    Update images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -4,7 +4,7 @@ namespace myImages {
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "image1":
-            case "myImage":return img`
+            case "Scissors":return img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 2 2 . . . . . . . . . . . . . . 
@@ -20,6 +20,44 @@ namespace myImages {
 2 2 2 2 2 2 . . . . . . . 1 1 1 
 2 2 2 2 . . . . . . . . . . . 1 
 2 2 . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "Rock":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . f f f f f . . . . . . . 
+. . . . f d d 1 f f f f . . . . 
+. . f f 1 1 1 d d d 1 f f . . . 
+. . f d 1 d d d d d d d f . . . 
+. f f d d 1 1 1 1 1 d d f . . . 
+. f 1 d 1 1 d d d d d d f . . . 
+. f d d 1 1 1 d d d d d f . . . 
+. f f d d 1 d d 1 1 d d f . . . 
+. . f d d d 1 1 1 d 1 d f . . . 
+. . f f f 1 1 d d d d f f . . . 
+. . . . . f f d d d f f . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image3":
+            case "Paper":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . 1 1 1 . . . . . 
+. . . . . . . 1 1 1 1 1 . . . . 
+. . . . . . 1 1 f 1 1 1 1 1 . . 
+. . . . . 1 1 1 f f 1 1 1 1 . . 
+. . . . 1 1 1 1 1 1 f 1 1 . . . 
+. . . 1 1 1 f f 1 1 1 1 1 . . . 
+. . 1 1 1 1 1 f f 1 1 1 . . . . 
+. . 1 1 f 1 1 1 f 1 1 . . . . . 
+. . 1 1 f f 1 1 1 1 1 . . . . . 
+. . . 1 1 f f 1 1 1 . . . . . . 
+. . . . 1 1 1 1 1 . . . . . . . 
+. . . . . 1 1 1 . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `;
         }
